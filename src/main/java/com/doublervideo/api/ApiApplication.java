e5c83fdde0dc4c8004985e446a5eb5fc40a3b82c
Evan Rosen <evanrosen@me.com>
2020-02-04 19:15:18 -0700

    working

--- src/main/java/com/doublervideo/api/ApiApplication.java
+++ src/main/java/com/doublervideo/api/ApiApplication.java
@@ -1,9 +1,14 @@
 package com.doublervideo.api;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
 @SpringBootApplication
+
+@EnableAutoConfiguration(exclude = {SecurityAutoConfiguration.class})
+
 public class ApiApplication {
 
 	public static void main(String[] args) {
@@ -11,3 +16,5 @@ public class ApiApplication {
 	}
 
 }
+
+
