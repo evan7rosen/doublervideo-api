0dd606b6b233fc818ec414cc3ef983c7dfb59f00
Evan Rosen <evanrosen@me.com>
2020-02-15 14:08:35 -0700

    updated seed data

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,9 +1,9 @@
 INSERT INTO videos(title,url,date) VALUES
-('AZ Ice Peoria Summer Ice Show - Wish Upon a Star', 'https://drive.google.com/file/d/14R7FoEpID62BKHlXVhTGJQ9UrbNlA8IP/view?usp=sharing', 'May 26, 2018'),
-('AZ Ice Gilbert Holiday Ice Show - The Time Train', 'https://drive.google.com/file/d/1D3tapZ4ZXPkcTEnoPYnm9ht_EP6H1gMb/view?usp=sharing', 'December 7-8 2018'),
-('Ice Den 20th Anniversary Ice Show', 'https://drive.google.com/file/d/1An_vujhcv64-OLfguMoWjbPJoqOYkZB6/view?usp=sharing', 'June 16, 2018'),
-('Ice Den Summer Ice Show - Grammys on Ice', 'https://drive.google.com/file/d/1cQgxM-dR5AkgMMkVWKkXRjFliMZrroS0/view?usp=sharing', 'June 15, 2019'),
-('AZ Ice Gilbert Holiday Ice Show - A Christmas Fantasy', 'https://drive.google.com/file/d/14R7FoEpID62BKHlXVhTGJQ9UrbNlA8IP/view?usp=sharing', 'December 15, 2018');
+('AZ Ice Peoria Summer Ice Show - Wish Upon a Star', 'https://drive.google.com/file/d/18kKUH2p5Fg1hQBEmCBC6zm7Gq2UN9GRU', 'May 26, 2018'),
+('AZ Ice Gilbert Holiday Ice Show - The Time Train', 'https://drive.google.com/file/d/1D3tapZ4ZXPkcTEnoPYnm9ht_EP6H1gMb/preview', 'December 7-8 2018'),
+('Ice Den 20th Anniversary Ice Show', 'https://drive.google.com/file/d/1An_vujhcv64-OLfguMoWjbPJoqOYkZB6/preview', 'June 16, 2018'),
+('Ice Den Summer Ice Show - Grammys on Ice', 'https://drive.google.com/file/d/1cQgxM-dR5AkgMMkVWKkXRjFliMZrroS0/preview', 'June 15, 2019'),
+('AZ Ice Gilbert Holiday Ice Show - Aladdin on Ice', 'https://drive.google.com/file/d/1gEGlyNTTbHaagcUwc4tlRG5t7i4DsHxs/preview', 'December 14, 2019');
 
 INSERT INTO users(name,email,password) VALUES
 ('Evan Rosen', 'evan@doublervideo.com', 'DoubleRVid1'),
@@ -13,13 +13,6 @@ INSERT INTO users(name,email,password) VALUES
 ('Kristy Rosen', 'evan@doublervideo.com', 'DoubleRVid1');
 
 
-insert into users_videos (user_id, video_id) values (1, 1);
-insert into users_videos (user_id, video_id) values (2, 1);
-insert into users_videos (user_id, video_id) values (3, 2);
-insert into users_videos (user_id, video_id) values (4, 2);
-insert into users_videos (user_id, video_id) values (5, 3);
-insert into users_videos (user_id, video_id) values (1, 3);
-insert into users_videos (user_id, video_id) values (2, 4);
-insert into users_videos (user_id, video_id) values (3, 4);
-insert into users_videos (user_id, video_id) values (4, 5);
-insert into users_videos (user_id, video_id) values (5, 5);
+insert into users_videos (user_id, video_id) VALUES
+ (1, 1), (2, 1), (3, 1), (4, 1), (5, 1), (1, 2), (2, 2), (3, 2), (4, 2), (5, 2),
+ (1, 3), (2, 3), (3, 3), (4, 3), (5, 3), (1, 4), (1, 5), (2, 4), (2, 5), (3, 4)
