a61e83c27c518fc0184d34951f5343e8e60baf42
Evan Rosen <evanrosen@me.com>
2020-03-03 10:26:35 -0700

    updated seed data'

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,9 +1,11 @@
 INSERT INTO videos(title,url,date) VALUES
-('AZ Ice Peoria Summer Ice Show - Wish Upon a Star', '7uGhUWSmZZY', 'May 26, 2018'),
-('AZ Ice Gilbert Holiday Ice Show - The Time Train', 'QrR_gm6RqCo', 'December 7-8 2018'),
-('Ice Den 20th Anniversary Ice Show', '7TML_MTQdg4', 'June 16, 2018'),
-('Ice Den Summer Ice Show - Grammys on Ice', 'https://drive.google.com/file/d/1cQgxM-dR5AkgMMkVWKkXRjFliMZrroS0/preview', 'June 15, 2019'),
-('AZ Ice Gilbert Holiday Ice Show - Aladdin on Ice', 'https://drive.google.com/file/d/1gEGlyNTTbHaagcUwc4tlRG5t7i4DsHxs/preview', 'December 14, 2019');
+('Ice Den Scottsdale Summer Ice Show - Boots and Blades', 'y_G-lOh12p8', 'June 10, 2017'),
+('Ice Den Chandler Holiday Ice Spectacular', '76Fen4BEhgo', 'December 21, 2019'),
+('Ice Den Scottsdale 20th Anniversary Ice Show', 'dOxj8Xv3fp4', 'June 16, 2018'),
+('Julian Gil at Desert Breeze Skate Championships', 'K6rNq19SURM', 'June 16, 2013'),
+('AZ Ice Gilbert Holiday Ice Show - Aladdin on Ice', 'KVWBjEg3yxI', 'December 14, 2019'),
+('Ice Den Scottsdale Summer Ice Show - Love', 'y_G-lOh12p8', 'June 5, 2018'),
+('Ice Den Scottsdale Holiday Ice Spectacular', '76Fen4BEhgo', 'December 18, 2018')
 
 INSERT INTO users(name,email,password) VALUES
 ('Evan Rosen', 'evan@doublervideo.com', 'DoubleRVid1'),
@@ -13,6 +15,6 @@ INSERT INTO users(name,email,password) VALUES
 ('Kristy Rosen', 'evan@doublervideo.com', 'DoubleRVid1');
 
 
---insert into users_videos (user_id, video_id) VALUES
--- (1, 1), (2, 1), (3, 1), (4, 1), (5, 1), (1, 2), (2, 2), (3, 2), (4, 2), (5, 2),
--- (1, 3), (2, 3), (3, 3), (4, 3), (5, 3), (1, 4), (1, 5), (2, 4), (2, 5), (3, 4)
+insert into users_videos (user_id, video_id) VALUES
+ (1, 1), (2, 1), (3, 1), (4, 1), (5, 1), (1, 2), (2, 2), (3, 2), (4, 2), (5, 2),
+ (1, 3), (2, 3), (3, 3), (4, 3), (5, 3)
